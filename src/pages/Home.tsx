5803ca8f71a75af371a7b48e65138a08bf57589b
estebanvivanco <esteban.vivanco.progra@gmail.com>
2025-05-14 12:13:17 -0400

    feat: cambio en home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,7 @@
 import { useEffect } from "react";
 import { useBreadcrumbContext } from "../contexts/breadCrumbContext";
 // import Dashboard from "../components/Dashboard/Dashboard";
-import HeaderDescripcion from "../components/shared/HeaderDescripcion";
-import dashboard_photo from "../media/temporal/dashboard_photo.png";
-
+import logo from '../media/logos/alatheia-logo-dark.svg';
 import { motion } from "motion/react";
 
 const Home: React.FC = () => {
@@ -19,11 +17,18 @@ const Home: React.FC = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
-      <HeaderDescripcion
-        title="Home"
-        description="Aqui puedes ver el reporte historico y mensual"
-        photo_path={dashboard_photo}
-      />
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+
+        <div className="flex justify-center my-10">
+        <img src={logo} alt="Dashboard" className="w-1/3" />
+        </div>
+        <h2 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl mt-4 mb-4">Bienvenid@ al portal de Comodatos</h2>
+        <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+          Dentro de este portal podrás gestionar tus comodatos, ver el estado de tus clientes y mucho más.
+        </p>
+      </div>
+    </div>
 
       
 
